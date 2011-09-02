9503932021cba30802365d04793c77e3e43e8c37
Christopher Mark Gore <cgore@cgore.com>
2011-09-01 20:48:14 -0500

    Moving lisp-utilities to utilities.

--- tvm.lisp
+++ tvm.lisp
@@ -31,7 +31,7 @@
 ;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;;; POSSIBILITY OF SUCH DAMAGE.
 
-(load "lisp-utilities/utilities")
+(load "utilities/utilities")
 (use-package 'utilities)
 
 (defun future-value-formula (present-value future-value interest% periods)
